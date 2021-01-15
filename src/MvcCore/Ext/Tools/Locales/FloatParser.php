5d2304c042f071b8d4048ba7d7c0c5a0270879a2
Tom Flídr <tomflidr@gmail.com>
2021-01-15 22:46:19 +0100

    Fixed copyright heading, fixed php docs comments with unnecessary interfaces, upgraded version constant to 5.0.0

--- src/MvcCore/Ext/Tools/Locales/FloatParser.php
+++ src/MvcCore/Ext/Tools/Locales/FloatParser.php
@@ -7,7 +7,7 @@
  * For the full copyright and license information, please view
  * the LICENSE.md file that are distributed with this source code.
  *
- * @copyright	Copyright (c) 2016 Tom Flídr (https://github.com/mvccore/mvccore)
+ * @copyright	Copyright (c) 2016 Tom Flidr (https://github.com/mvccore)
  * @license		https://mvccore.github.io/docs/mvccore/5.0.0/LICENCE.md
  */
 
@@ -19,14 +19,14 @@ namespace MvcCore\Ext\Tools\Locales;
  *					floating point detection (which looks more successful).
  *					If there is not possible to get `float` value, return `NULL`.
  */
-class FloatParser
-{
+class FloatParser {
+
 	/**
 	 * MvcCore - version:
 	 * Comparison by PHP function `version_compare();`.
 	 * @see http://php.net/manual/en/function.version-compare.php
 	 */
-	const VERSION = '5.0.0-alpha';
+	const VERSION = '5.0.0';
 
 	/**
 	 * Language international code, lower case, example: `"en" | "de"`.
